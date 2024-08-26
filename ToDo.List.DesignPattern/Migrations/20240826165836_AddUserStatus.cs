a0cb0d3b092b97d02f7d2a5694fc8b1e3965fac5
Otavio Nascimento <otavio_nascimento@hotmail.com>
2024-08-26 13:59:19 -0300

    Add User Credentians to TodoList

---
+++ ToDo.List.DesignPattern/Migrations/20240826165836_AddUserStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.List.DesignPattern.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+        }
+    }
+}
